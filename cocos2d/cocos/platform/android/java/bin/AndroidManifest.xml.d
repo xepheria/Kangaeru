069afe2f83bbe8124e8960bd6cb5ca5cc9877d4d
Alex <ajy@clemson.edu>
2014-11-12 00:07:03 -0500

    unecessary directories

--- cocos2d/cocos/platform/android/java/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/alex/Kangaeru/Kangaeru/cocos2d/cocos/platform/android/java/bin/AndroidManifest.xml : \
-/home/alex/Kangaeru/Kangaeru/cocos2d/cocos/platform/android/java/AndroidManifest.xml \
--- cocos2d/cocos/platform/android/java/gen/R.java.d
+++
@@ -1,1 +0,0 @@
- : /home/alex/Kangaeru/Kangaeru/cocos2d/cocos/platform/android/java/bin/AndroidManifest.xml \
